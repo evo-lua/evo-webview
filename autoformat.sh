e74273d6663f6bff92c36f4a4f5f806f1a846fa7
RDW <duckwhale@mail.ru>
2023-06-01 18:19:16 +0200

    Repo: Add an autoformat script for local development
    The clang-format CLI is cumbersome at best, so this will make life a little easier.

---
+++ autoformat.sh
@@ -0,0 +1,13 @@
+echo "Discovering sources ..."
+
+RELEVANT_FILES_TO_FORMAT=$(find . -type f \( -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "*.hpp" -o -name "*.mm" \) -print -o -path "*/deps" -prune -o -path "*/cmakebuild*" -prune)
+
+if [ -n "$RELEVANT_FILES_TO_FORMAT" ]; then
+	echo "Discovered sources:"
+	echo $RELEVANT_FILES_TO_FORMAT
+
+	echo "Formatting sources ..."
+	clang-format -i --verbose $RELEVANT_FILES_TO_FORMAT
+else
+	echo "NO relevant sources found"
+fi
