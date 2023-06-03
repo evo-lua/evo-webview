6cb97d688a2f037437fad664c05b336f74d52b44
RDW <duckwhale@mail.ru>
2023-06-03 19:33:31 +0200

    Refactor: Split the webview base class into cpp and hpp

--- src/common/webview.hpp
+++ src/common/webview.hpp
@@ -12,7 +12,11 @@
 #endif
 #endif
 
-#include "json_utils.hpp"
+#include <functional>
+#include <map>
+#include <string>
+
+#include "json_utils.hpp" // Very sketchy since this isn't part of the public API, but it is what it is (for now)
 
 #if defined(WEBVIEW_GTK)
 #include "webkit2gtk_engine.hpp"
@@ -26,22 +30,14 @@ namespace webview {
 
 class webview : public browser_engine {
 public:
-  webview(bool debug = false, void *wnd = nullptr)
-      : browser_engine(debug, wnd) {}
+  webview(bool debug = false, void *wnd = nullptr);
 
-  void navigate(const std::string &url) {
-    if (url.empty()) {
-      browser_engine::navigate("about:blank");
-      return;
-    }
-    browser_engine::navigate(url);
-  }
+  void navigate(const std::string &url);
 
   using binding_t = std::function<void(std::string, std::string, void *)>;
   class binding_ctx_t {
   public:
-    binding_ctx_t(binding_t callback, void *arg)
-        : callback(callback), arg(arg) {}
+    binding_ctx_t(binding_t callback, void *arg);
     // This function is called upon execution of the bound JS function
     binding_t callback;
     // This user-supplied argument is passed to the callback
@@ -49,76 +45,18 @@ public:
   };
 
   using sync_binding_t = std::function<std::string(std::string)>;
-
   // Synchronous bind
-  void bind(const std::string &name, sync_binding_t fn) {
-    auto wrapper = [this, fn](const std::string &seq, const std::string &req,
-                              void * /*arg*/) { resolve(seq, 0, fn(req)); };
-    bind(name, wrapper, nullptr);
-  }
+  void bind(const std::string &name, sync_binding_t fn);
 
   // Asynchronous bind
-  void bind(const std::string &name, binding_t fn, void *arg) {
-    if (bindings.count(name) > 0) {
-      return;
-    }
-    bindings.emplace(name, binding_ctx_t(fn, arg));
-    auto js = "(function() { var name = '" + name + "';" + R""(
-      var RPC = window._rpc = (window._rpc || {nextSeq: 1});
-      window[name] = function() {
-        var seq = RPC.nextSeq++;
-        var promise = new Promise(function(resolve, reject) {
-          RPC[seq] = {
-            resolve: resolve,
-            reject: reject,
-          };
-        });
-        window.external.invoke(JSON.stringify({
-          id: seq,
-          method: name,
-          params: Array.prototype.slice.call(arguments),
-        }));
-        return promise;
-      }
-    })())"";
-    init(js);
-    eval(js);
-  }
+  void bind(const std::string &name, binding_t fn, void *arg);
 
-  void unbind(const std::string &name) {
-    auto found = bindings.find(name);
-    if (found != bindings.end()) {
-      auto js = "delete window['" + name + "'];";
-      init(js);
-      eval(js);
-      bindings.erase(found);
-    }
-  }
+  void unbind(const std::string &name);
 
-  void resolve(const std::string &seq, int status, const std::string &result) {
-    dispatch([seq, status, result, this]() {
-      if (status == 0) {
-        eval("window._rpc[" + seq + "].resolve(" + result +
-             "); delete window._rpc[" + seq + "]");
-      } else {
-        eval("window._rpc[" + seq + "].reject(" + result +
-             "); delete window._rpc[" + seq + "]");
-      }
-    });
-  }
+  void resolve(const std::string &seq, int status, const std::string &result);
 
 private:
-  void on_message(const std::string &msg) {
-    auto seq = json::json_parse(msg, "id", 0);
-    auto name = json::json_parse(msg, "method", 0);
-    auto args = json::json_parse(msg, "params", 0);
-    auto found = bindings.find(name);
-    if (found == bindings.end()) {
-      return;
-    }
-    const auto &context = found->second;
-    context.callback(seq, args, context.arg);
-  }
+  void on_message(const std::string &msg);
 
   std::map<std::string, binding_ctx_t> bindings;
 };
